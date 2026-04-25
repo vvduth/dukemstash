ae5c9808161bb1aa3af8198a4eb70aee1902933b
vvduth <ducthai060501@gmail.com>
2026-04-25 20:17:41 +0300

    pass isPro props into ItemGridWithDrawer for ai explain feature.

--- src/app/dashboard/items/[type]/page.tsx
+++ src/app/dashboard/items/[type]/page.tsx
@@ -91,6 +91,7 @@ export default async function ItemsTypePage({
           <ItemGridWithDrawer
             items={items}
             collections={userCollections}
+            isPro={isPro}
             className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3"
           />
         )
